d6e89b3442bb065ae54f10e8dff2935afcfe1f88
Prashant Pandey <23prashantpandey@gmail.com>
2025-11-18 20:26:44 +0530

    Add files via upload

---
+++ C Program 17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    int b = 17;
+    int result;
+
+    printf("The result of the bitwise AND operation on %d and %d is: %d\n", a, b, result);
+
+    return 0;
+}
