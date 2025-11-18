133fb754abdb948f96afdacbe63b4704a4ca29df
Prashant Pandey <23prashantpandey@gmail.com>
2025-11-18 20:27:03 +0530

    Add files via upload

---
+++ C Program 18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;
+    int b = 17;
+    int result;
+
+    result = a | b;
+
+    printf("The result of a|b is: %d\\n", a, b, result);
+
+    return 0;
+}
