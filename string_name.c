756c9693d172020a4fa03a33cc43d576a993cb38
Prashant Pandey <23prashantpandey@gmail.com>
2025-12-24 10:25:13 +0530

    Add files via upload

---
+++ string_name.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[10]={'P','R','A','S','H','A','N','T'};
+    int i;
+    for(i=0; i<=10; i++)
+    {
+    printf("%c", name[i]);
+    }
+    return 0;
+}
